101d047cabce88d63d6666ef7354d27fd940a42b
jsoulis <jtsoulis7@gmail.com>
2019-08-14 16:14:20 +0900

    update code to hook in with pi web api endpoint

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ console.log("Hello world")
 
 async function myfetch() {
     //Outside web api call is made
-    //let response = await fetch("http://localhost:5590/api/v1/tenants/default/namespaces/default/streams/OpcUa5.Sinusoid1/Data?startIndex=2019-08-11T13:00:00Z&count=100");
-    //return await response.json();
+    let response = await fetch("http://localhost:5590/api/v1/tenants/default/namespaces/default/streams/OpcUa5.Sinusoid1/Data?startIndex=2019-08-11T13:00:00Z&count=100");
+    return await response.json();
 
-    let response = await getFirstBulkData(20);
-    return await response;
+    //let response = await getFirstBulkData(20);
+    //return await response;
 }
 
 var ctx = document.getElementById('chart1').getContext('2d');
@@ -68,8 +68,7 @@ async function formatData() {
     let data = await myfetch();
     for(let i = 0; i < data.length; i++) {
         let item = {
-            t: data[i].Timestamp,
-            //t: Date.parse(data[i].Timestamp),
+            t: Date.parse(data[i].Timestamp),
             y: data[i].Value
         };
         formatData.push(item);
@@ -91,34 +90,35 @@ async function AsyncInitialize() {
 
 const formatDate = (data) => {
     let dataOutput = {
-        t: data.Timestamp,
+        //t: data.Timestamp,
+        t: Date.parse(data.Timestamp),
         y: data.Value
     }
     return dataOutput;
 }
 const updateChart = (dataInput) => {
+    console.log(dataInput);
     var data = chart.config.data.datasets[0].data;
-    data.pop();
-    data.unshift(formatDate(dataInput));
-
-    console.log(data);
-    chart.update();
+    if(data[data.length-1] !== dataInput) {
+        data.shift();
+        data.push(formatDate(dataInput));
+        chart.update();
+    }
+ 
 }
 
 async function asyncUpdate() {
     //will need to do something different here to periodically poll api last endpoint for stream.
     //will simulate this behavior in the following example.
 
-    let dataUpdate = await getSimulatorData();
-    updateChart(dataUpdate);
-    setTimeout(asyncUpdate, 5000);
+    //let dataUpdate = await getSimulatorData();
+    let dataUpdate = await fetch("http://localhost:5590/api/v1/tenants/default/namespaces/default/streams/OpcUa5.Sinusoid1/data/last")
+    let dataItem = await dataUpdate.json();
+    updateChart(dataItem);
+
+    setTimeout(asyncUpdate, 500);
 }
 
-const fullChartUpdate = () => {
-    //update entire graph at once.
-    let data = chart.config.data.datasets[0].data;
-
-}
 
 AsyncInitialize();
 asyncUpdate();
